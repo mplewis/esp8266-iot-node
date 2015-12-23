632b5b7a43768722c57e56a4681e4ded63649fa1
Matthew Lewis <matt@mplewis.com>
2015-12-22 23:17:01 -0600

    matrix: scroll hello world on startup

---
+++ src/led_matrix.h
@@ -0,0 +1,50 @@
+#include <HT1632.h>
+#include <font_8x4.h>
+#include <images.h>
+
+const char PIN_LED_MATRIX_DATA = D0;
+const char PIN_LED_MATRIX_WR = D1;
+const char PIN_LED_MATRIX_RD = D2;
+const char PIN_LED_MATRIX_CS = D3;
+
+char *text;
+int text_width;
+int text_pos;
+int next_scroll_at = 0;
+
+void start_led_matrix() {
+  HT1632.begin(PIN_LED_MATRIX_CS, PIN_LED_MATRIX_WR, PIN_LED_MATRIX_DATA);
+  HT1632.clear();
+}
+
+void handle_led_matrix() {
+  if (next_scroll_at == 0 || millis() < next_scroll_at) return;
+  if (text_pos > text_width + OUT_SIZE) {
+    next_scroll_at = 0;
+    return;
+  }
+
+  next_scroll_at = millis() + TEXT_SCROLL_DELAY;
+  int x = OUT_SIZE - text_pos;
+  int y = 0;
+  HT1632.drawText(text, x, y, FONT_8X4, FONT_8X4_END, FONT_8X4_HEIGHT);
+  HT1632.render();
+  text_pos++;
+}
+
+void show_heart() {
+  HT1632.begin(PIN_LED_MATRIX_CS, PIN_LED_MATRIX_WR, PIN_LED_MATRIX_DATA);
+  // The definitions for IMG_HEART and its width and height are available in
+  // images.h.
+  // This step only performs the drawing in internal memory.
+  HT1632.drawImage(IMG_HEART, IMG_HEART_WIDTH, IMG_HEART_HEIGHT,
+                   (OUT_SIZE - IMG_HEART_WIDTH) / 2, 0);
+  HT1632.render();  // This updates the display on the screen.
+}
+
+void scroll_text(char *t) {
+  text = t;
+  text_width = HT1632.getTextWidth(t, FONT_8X4_END, FONT_8X4_HEIGHT);
+  text_pos = 0;
+  next_scroll_at = millis();
+}
--- src/sketch.ino
+++ src/sketch.ino
@@ -1,15 +1,19 @@
 #include "wifi.h"
 #include "cmds.h"
+#include "led_matrix.h"
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
 
   start_leds();
+  start_led_matrix();
   start_wifi();
+  scroll_text("Hello world!");
 }
 
 void loop() {
   handle_leds();
+  handle_led_matrix();
   handle_wifi();
 }
