0d0ec2b7aa8a99eb3e81efe2b8a4a34ccd6d66c8
Matthew Lewis <matt@mplewis.com>
2015-12-23 00:03:25 -0600

    matrix: clear before print; copy char*; string support

--- src/led_matrix.h
+++ src/led_matrix.h
@@ -1,3 +1,6 @@
+#ifndef __led_matrix_h
+#define __led_matrix_h
+
 #include <HT1632.h>
 #include <font_tinyfont.h>
 #include <images.h>
@@ -7,7 +10,9 @@ const char PIN_LED_MATRIX_WR = D1;
 const char PIN_LED_MATRIX_RD = D2;
 const char PIN_LED_MATRIX_CS = D3;
 
-const char *text;
+const int TEXT_MAX_CHARS = 256;
+
+char text[TEXT_MAX_CHARS];
 int text_width;
 int text_pos;
 int next_scroll_at = 0;
@@ -38,21 +43,28 @@ void handle_led_matrix() {
 }
 
 void show_heart() {
-  HT1632.begin(PIN_LED_MATRIX_CS, PIN_LED_MATRIX_WR, PIN_LED_MATRIX_DATA);
-  // The definitions for IMG_HEART and its width and height are available in
-  // images.h.
-  // This step only performs the drawing in internal memory.
+  HT1632.clear();
   HT1632.drawImage(IMG_HEART, IMG_HEART_WIDTH, IMG_HEART_HEIGHT,
                    (OUT_SIZE - IMG_HEART_WIDTH) / 2, 0);
-  HT1632.render();  // This updates the display on the screen.
+  HT1632.render();
 }
 
-void _set_scroll_text(const char *t, bool repeat) {
-  text = t;
-  text_width = HT1632.getTextWidth(t, FONT_TINYFONT_END, FONT_TINYFONT_HEIGHT);
+void _finalize_scroll_text(bool repeat) {
+  text_width = HT1632.getTextWidth(text, FONT_TINYFONT_END, FONT_TINYFONT_HEIGHT);
   text_pos = 0;
   next_scroll_at = millis();
   scroll_repeat = repeat;
+  HT1632.clear();
+}
+
+void _set_scroll_text(const char *t, bool repeat) {
+  strncpy(text, t, TEXT_MAX_CHARS);
+  _finalize_scroll_text(repeat);
+}
+
+void _set_scroll_text(String s, bool repeat) {
+  s.toCharArray(text, TEXT_MAX_CHARS);
+  _finalize_scroll_text(repeat);
 }
 
 void scroll_once(const char *t) {
@@ -62,3 +74,13 @@ void scroll_once(const char *t) {
 void scroll_forever(const char *t) {
   _set_scroll_text(t, true);
 }
+
+void scroll_once(String s) {
+  _set_scroll_text(s, false);
+}
+
+void scroll_forever(String s) {
+  _set_scroll_text(s, true);
+}
+
+#endif
