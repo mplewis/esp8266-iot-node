0fd1815a2b66627456cebeecc5765a44ec8bbd27
Matthew Lewis <matt@mplewis.com>
2015-12-23 20:15:48 -0600

    http: stop parsing headers; copy strings, not mangle them

--- src/cmds.h
+++ src/cmds.h
@@ -1,64 +1,39 @@
-const int REQUEST_LINE_SIZE = 256;
-const char HEADER_COUNT = 64;
+#include <math.h>
 
-char *request_line;
-char *headers[HEADER_COUNT] = {0};
-char *body;
+const int REQUEST_LINE_SIZE = 256;
+const int BODY_SIZE = 1024;
 
 void parse_headers(char *buf);
-void print_request(char *request_line, char *headers[],
-                   int header_count, char *body);
-void debug_request(char *request_line, char *headers[],
-                   int header_count, char *body);
+void handle_request(char *request_line, char *body);
+void debug_request(char *request_line, char *body);
 
 void parse_request(char *buf) {
-  char *headers_end = strstr(buf, "\r\n\r\n");
-  body = headers_end + 4;
-  headers_end[0] = 0;  // Terminate headers
-  parse_headers(buf);
+  char request_line[REQUEST_LINE_SIZE];
+  char body[BODY_SIZE];
+
+  // Headers start right after the request line
+  char *request_line_end = strstr(buf, "\r\n");
+  // Add 1 byte for the terminator
+  int request_line_len = request_line_end - buf + 1;
+  strlcpy(request_line, buf, fmin(request_line_len, REQUEST_LINE_SIZE));
+
+  // Body starts right after the headers end with a blank line
+  char *body_start = strstr(buf, "\r\n\r\n") + 4;
+  strlcpy(body, body_start, BODY_SIZE);
+
+  handle_request(request_line, body);
 }
 
-void parse_headers(char *buf) {
-  // parse http headers, tokenize by \r\n
-  const char *header_sep = "\r\n";
-  bool is_request_line = true;
-  int header_num = 0;
-
-  char *token = strtok(buf, header_sep);
-  while (token != 0 && header_num < HEADER_COUNT) {
-    if (is_request_line) {
-      request_line = token;
-      is_request_line = false;
-    } else {
-      headers[header_num++] = token;
-    }
-    token = strtok(0, header_sep);
+void handle_request(char *request_line, char *body) {
+  if (WIFI_DEBUG_REQUESTS) {
+    debug_request(request_line, body); 
   }
-
-  print_request(request_line, headers, header_num, body);
-}
-
-void print_request(char *request_line, char *headers[],
-                   int header_count, char *body) {
-
-  if (WIFI_DEBUG_REQUESTS)
-    debug_request(request_line, headers, header_count, body); 
-
   scroll_once(body);
 }
 
-void debug_request(char *request_line, char *headers[],
-                   int header_count, char *body) {
+void debug_request(char *request_line, char *body) {
   Serial.println("Request line:");
   Serial.println(request_line);
-  Serial.println();
-
-  Serial.println("Headers:");
-  for (int i = 0; i < header_count; i++) {
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.println(headers[i]);
-  }
   Serial.println();
 
   Serial.println("Body:");
